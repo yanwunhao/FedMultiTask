3f951490a15a075fdf6151ec58e6da0c30a9dc19
十裏山路不換肩 <64908450+yanwunhao@users.noreply.github.com>
2024-08-20 14:49:43 +0900

    finish dataset io

---
+++ util/dataset.py
@@ -0,0 +1,74 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class FEMNIST(Dataset):
+    """
+    This dataset is derived from the Leaf repository
+    (https://github.com/TalwalkarLab/leaf) pre-processing of the Extended MNIST
+    dataset, grouping examples by writer. Details about Leaf were published in
+    "LEAF: A Benchmark for Federated Settings" https://arxiv.org/abs/1812.01097.
+    """
+
+    def __init__(
+        self,
+        train=True,
+        transform=None,
+        target_transform=None,
+    ):
+        super(FEMNIST, self).__init__()
+        self.transform = transform
+        self.target_transform = target_transform
+        self.train = train
+
+        train_clients, train_groups, train_data_temp, test_data_temp = read_data(
+            "./data/femnist/train", "./data/femnist/test"
+        )
+        if self.train:
+            self.dic_users = {}
+            train_data_x = []
+            train_data_y = []
+            for i in range(len(train_clients)):
+                # if i == 100:
+                #     break
+                self.dic_users[i] = set()
+                l = len(train_data_x)
+                cur_x = train_data_temp[train_clients[i]]["x"]
+                cur_y = train_data_temp[train_clients[i]]["y"]
+                for j in range(len(cur_x)):
+                    self.dic_users[i].add(j + l)
+                    train_data_x.append(np.array(cur_x[j]).reshape(28, 28))
+                    train_data_y.append(cur_y[j])
+            self.data = train_data_x
+            self.label = train_data_y
+        else:
+            test_data_x = []
+            test_data_y = []
+            for i in range(len(train_clients)):
+                cur_x = test_data_temp[train_clients[i]]["x"]
+                cur_y = test_data_temp[train_clients[i]]["y"]
+                for j in range(len(cur_x)):
+                    test_data_x.append(np.array(cur_x[j]).reshape(28, 28))
+                    test_data_y.append(cur_y[j])
+            self.data = test_data_x
+            self.label = test_data_y
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.label[index]
+        img = np.array([img])
+        # img = Image.fromarray(img, mode='L')
+        # if self.transform is not None:
+        #     img = self.transform(img)
+        # if self.target_transform is not None:
+        #     target = self.target_transform(target)
+        return torch.from_numpy((0.5 - img) / 0.5).float(), target
+
+    def __len__(self):
+        return len(self.data)
+
+    def get_client_dic(self):
+        if self.train:
+            return self.dic_users
+        else:
+            exit("The test dataset do not have dic_users!")
---
+++ util/io.py
@@ -0,0 +1,53 @@
+import os, json
+from collections import defaultdict
+
+
+def read_dir(data_dir):
+    x_data = []
+    y_data = []
+    c_data = []
+
+    clients = []
+    data = defaultdict(None)
+
+    files = os.listdir(data_dir)
+    files = [f for f in files if f.endswith(".json")]
+
+    for f in files:
+        full_path = os.path.join(data_dir, f)
+        with open(full_path, "r") as f_content:
+            cdata = json.load(f_content)
+            clients.extend(cdata["users"])
+            data.update(cdata["user_data"])
+
+    for client in clients:
+        client_data = data[client]
+        if len(client_data["x"]) == len(client_data["y"]):
+            client_data_length = len(client_data["x"])
+            for i in range(client_data_length):
+                x_data.append(client_data["x"][i])
+                y_data.append(client_data["y"][i])
+                c_data.append(client)
+        else:
+            print("Fuck!")
+            return
+
+    if len(x_data) == len(y_data) == len(c_data):
+        return x_data, y_data, c_data
+    else:
+        print("Fuck!")
+        return
+
+
+def read_data(train_data_dir, test_data_dir):
+    train_x, train_labels, train_clients = read_dir(train_data_dir)
+    test_x, test_labels, test_clients = read_dir(test_data_dir)
+
+    train_set = set(train_clients)
+    test_set = set(test_clients)
+
+    return train_x, train_labels, train_clients, test_x, test_labels, test_clients
+
+
+if __name__ == "__main__":
+    read_data("./data/femnist/train", "./data/femnist/test")
