3256074bb9e6912bc3b44683f4d4bb9ad2da1045
十裏山路不換肩 <wh.yan@hotmail.com>
2024-10-15 15:35:53 +0900

    update

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import random
 
 import torch
 from torch.utils.data import DataLoader
+import numpy as np
 import matplotlib.pyplot as plt
 
 from util.dataset import FEMNIST
+from model.Nets import CNNFemnist
 
 RANDOM_SEED = 12345
 
@@ -27,7 +29,20 @@ if __name__ == "__main__":
     training_data = FEMNIST(train=True)
     test_data = FEMNIST(train=False)
 
-    train_dataloader = DataLoader(training_data, batch_size=32, shuffle=True)
-    test_dataloader = DataLoader(test_data, batch_size=32, shuffle=False)
+    train_dataloader = DataLoader(training_data, batch_size=16, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=16, shuffle=False)
+
+    model = CNNFemnist("")
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
     # training
+    num_epochs = 50
+    for round in range(num_epochs):
+        max_value = 0
+        for batch_idx, (x, y, c) in enumerate(test_dataloader):
+
+            pred = model(x)
+            print(pred.shape)
+
+        break
---
+++ model/Nets.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+
+
+class CNNFemnist(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 7, padding=3)
+        self.act = nn.ReLU()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.out = nn.Linear(64 * 7 * 7, 62)
+
+    def forward(self, x):
+        x = x.reshape(-1, 1, 28, 28)
+        x = self.pool(self.act(self.conv1(x)))
+        x = self.pool(self.act(self.conv2(x)))
+        x = x.flatten(1)
+        return self.out(x)
